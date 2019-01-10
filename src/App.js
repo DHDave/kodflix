9ebba67e20f905c6599518c8887f281f1e7de3dd
David Hajos <dh.it.email@gmail.com>
2019-01-10 22:18:56 +0000

    6 # Animate the TV show title overlay

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
           <div className='item'>
             <img src={starWarsVII} alt='StarWars seven' />
             <div className='overlay'>
-              <h1>Star Wars: The Force Awakens</h1>
+              <h1>Star Wars VII </h1>
             </div>
           </div>
         </div>
@@ -36,7 +36,7 @@ class App extends Component {
           <div className='item'>
             <img src={theBourneU} alt='the Bourne Ultimatum' />
             <div className='overlay'>
-              <h1>The Bourne Ultimatum'</h1>
+              <h1>The Bourne Ultimatum</h1>
             </div>
           </div>
           <div className='item'>
