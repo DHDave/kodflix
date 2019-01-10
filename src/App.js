973b183ae083b576d335a89c667dc02dffb05b5d
David Hajos <dh.it.email@gmail.com>
2019-01-10 15:05:04 +0000

    2 # Replace TV show titles with covers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import StarWars_img from './StarWars.jpg';
+import thirteenReasonsWhyImage from './images/13 Reason Why.jpeg';
+import arrow from './images/Arrow.jpeg';
+import starWarsVII from './images/StarWarsVII.jpeg';
+import theBourneU from './images/TheBourneU.jpeg';
+import theGoodfather from './images/TheGoodfather.jpeg';
+import paulAofChrist from './images/PaulAofChrist.jpeg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <img src={StarWars_img} alt='Han Solo, Chewie, Ben and Luke' />
         <br /><br /><br />
-        <div className= 'container'>
-          <div className='item'>ReactJS</div>
-          <div className='item'>HTML5</div>
-          <div className='item'>CSS3</div>
+        <div className='container'>
+          <div className='item'>
+            <img src={thirteenReasonsWhyImage} alt='13 Reasons Why' />
+          </div>
+          <div className='item'>
+            <img src={arrow} alt='Arrow' />
+          </div>
+          <div className='item'>
+            <img src={starWarsVII} alt='StarWars seven' />
+          </div>
         </div>
-        <div className= 'container'>
-          <div className='item'>Node.js</div>
-          <div className='item'>MongoDB</div>
-          <div className='item'>Heroku</div>
+        <div className='container'>
+          <div className='item'>
+            <img src={theBourneU} alt='the Bourne Ultimatum' />
+          </div>
+          <div className='item'>
+            <img src={theGoodfather} alt='The Goodfather' />
+          </div>
+          <div className='item'>
+            <img src={paulAofChrist} alt='Paul Apostol of Christ' />
+          </div>
         </div>
       </div>
     );
+   }
   }
-}
-
+              
 export default App;
