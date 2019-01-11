590f70ffce9466d7b5d156a7bff780ac642acb1d
David Hajos <dh.it.email@gmail.com>
2019-01-11 14:57:27 +0000

    9 # Create a Gallery component to show the covers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import Stack from './Stack';
-import thirteenReasonsWhyImage from './images/13 Reason Why.jpeg';
-import arrow from './images/Arrow.jpeg';
-import starWarsVII from './images/StarWarsVII.jpeg';
-import theBourneU from './images/TheBourneU.jpeg';
-import theGoodfather from './images/TheGoodfather.jpeg';
-import paulAofChrist from './images/PaulAofChrist.jpeg';
+import Technologies from './Technologies';
 
 import './App.css';
 
@@ -14,20 +8,12 @@ class App extends Component {
     return (
       <div className="App">
         <br /><br /><br />
-        <div className='container'>
-          <Stack name='13 Reasons Why' img={thirteenReasonsWhyImage} />
-          <Stack name='Arrow' img={arrow} />
-          <Stack name='Star Wars VII' img={starWarsVII} />
-          </div>
-        <div className='container'>
-          <Stack name='The Bourne Ultimatum' img={theBourneU} />
-          <Stack name='The Goodfather' img={theGoodfather} />
-          <Stack name='Paul Apostol of Christ' img={paulAofChrist} />
-        </div>
+        <Technologies />
       </div>
     );
   }
 }
+
 
 
 export default App;
---
+++ src/Technologies.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Stack from './Stack';
+import thirteenReasonsWhyImage from './images/13 Reason Why.jpeg';
+import arrow from './images/Arrow.jpeg';
+import starWarsVII from './images/StarWarsVII.jpeg';
+import theBourneU from './images/TheBourneU.jpeg';
+import theGoodfather from './images/TheGoodfather.jpeg';
+import paulAofChrist from './images/PaulAofChrist.jpeg';
+
+export default function Technologies(){
+    return (
+      <div>  
+        <div className='container'>
+          <Stack name='13 Reasons Why' img={thirteenReasonsWhyImage} />
+          <Stack name='Arrow' img={arrow} />
+          <Stack name='Star Wars VII' img={starWarsVII} />
+        </div>
+        <div className='container'>
+          <Stack name='The Bourne Ultimatum' img={theBourneU} />
+          <Stack name='The Goodfather' img={theGoodfather} />
+          <Stack name='Paul Apostol of Christ' img={paulAofChrist} />
+        </div>
+      </div>
+    );
+  }
