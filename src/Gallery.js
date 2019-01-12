d3977b89aef06ef40e031d462bc08ceb86dc4321
David Hajos <dh.it.email@gmail.com>
2019-01-12 20:03:54 +0000

    12 # Move the covers data model to a proper front-end data source

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import Stack from './Stack';
-import thirteenReasonsWhyImage from './images/13 Reason Why.jpeg';
-import arrow from './images/Arrow.jpeg';
-import starWarsVII from './images/StarWarsVII.jpeg';
-import theBourneU from './images/TheBourneU.jpeg';
-import theGoodfather from './images/TheGoodfather.jpeg';
-import paulAofChrist from './images/PaulAofChrist.jpeg';
+import getTitles from './titles-get';
 
-export default function Gallery(){
-    return (
-      <div>  
-        <div className='container'>
-          <Stack id='thirteen' name='13 Reasons Why' img={thirteenReasonsWhyImage} />
-          <Stack id='arrow' name='Arrow' img={arrow} />
-          <Stack id='starwarsvii' name='Star Wars VII' img={starWarsVII} />
-          <Stack id='thebourneultimatum' name='The Bourne Ultimatum' img={theBourneU} />
-          <Stack id='thegoodfather' name='The Goodfather' img={theGoodfather} />
-          <Stack id='paulapostolofchrist' name='Paul Apostol of Christ' img={paulAofChrist} />
-        </div>
+export default function Gallery() {
+  return (
+    <div>
+      <div className='container'>
+        {
+          getTitles().map(stack  => (
+            <Stack
+            key={stack.id}
+            id={stack.id}
+            name={stack.name}
+            img={stack.img} />
+          ))
+        }
       </div>
-    );
-  }
+    </div>
+  );
+}
+
---
+++ src/titles-get.js
@@ -0,0 +1,17 @@
+import thirteenReasonsWhyImage from './images/13 Reason Why.jpeg';
+import arrow from './images/Arrow.jpeg';
+import starWarsVII from './images/StarWarsVII.jpeg';
+import theBourneU from './images/TheBourneU.jpeg';
+import theGoodfather from './images/TheGoodfather.jpeg';
+import paulAofChrist from './images/PaulAofChrist.jpeg';
+
+export default function getTitles() {
+    return [
+      { id: 'thirteen', name: '13 Reasons Why', img: thirteenReasonsWhyImage },
+      { id: 'arrow', name :'Arrow', img: arrow },
+      { id: 'starwarsvii', name :'Star Wars VII', img: starWarsVII },
+      { id: 'thebourneultimatum', name: 'The Bourne Ultimatum', img: theBourneU },
+      { id: 'thegoodfather', name: 'The Goodfather', img: theGoodfather },
+      { id: 'paulapostolofchrist', name: 'Paul Apostol of Christ', img: paulAofChrist }
+    ];
+  }
