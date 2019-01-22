7e39801f1c2a7432a2fe6549f02b353884ddf928
David Hajos <dh.it.email@gmail.com>
2019-01-22 11:47:44 +0000

    Populating title detail pages

--- src/titles-get.js
+++ src/titles-get.js
@@ -7,11 +7,11 @@ import paulAofChrist from './images/PaulAofChrist.jpeg';
 
 export default function getTitles() {
   return [
-    { id: 'thirteen', name: '13 Reasons Why', img: thirteenReasonsWhyImage, details: '' },
-    { id: 'arrow', name: 'Arrow', img: arrow },
+    { id: 'thirteen', name: '13 Reasons Why', img: thirteenReasonsWhyImage, details: 'A classmate receives a series of tapes that unravel the mystery of a tenageg girls past.' },
+    { id: 'arrow', name: 'Arrow', img: arrow, details: 'After a violent shipwreck, billionaire playboy Oliver Queen was missing and presumed dead for five years before being discovered alive on a remote island in the North China Sea. He returned home to Star City, but sinister players with a connection to Olivers past will push the team to its breaking point, threatening his legacy as the Green Arrow.' },
     { id: 'starwarsvii', name: 'Star Wars VII', img: starWarsVII, details: 'Thirty years after the defeat of the Galactic Empire, the galaxy faces a new threat from the evil Kylo Ren (Adam Driver) and the First Order. When a defector named Finn (John Boyega) crash-lands on a desert planet, he meets Rey (Daisy Ridley), a tough scavenger whose droid contains a top-secret map. Together, the young duo joins forces with Han Solo (Harrison Ford) to make sure the Resistance receives the intelligence concerning the whereabouts of Luke Skywalker (Mark Hamill), the last of the Jedi Knights.'},
-    { id: 'thebourneultimatum', name: 'The Bourne Ultimatum', img: theBourneU, details: '' },
-    { id: 'thegoodfather', name: 'The Goodfather', img: theGoodfather, details: '' },
-    { id: 'paulapostolofchrist', name: 'Paul Apostol of Christ', img: paulAofChrist, details: '' }
+    { id: 'thebourneultimatum', name: 'The Bourne Ultimatum', img: theBourneU, details: 'Bourne races to discover the final mysteries of his past while a government agent tries to track him down after a shootout in Moscow.' },
+    { id: 'thegoodfather', name: 'The Goodfather', img: theGoodfather, details: 'Francis Ford Coppolas epic masterpiece features Marlon Brando in his Oscar-winning role as the patriarch of the Corleone family.' },
+    { id: 'paulapostolofchrist', name: 'Paul Apostol of Christ', img: paulAofChrist, details: 'Paul (Faulkner), who goes from the most infamous persecutor of Christians to Christ’s most influential apostle, is spending his last days in a dark and bleak prison cell awaiting execution by Emperor Nero. Luke(Caviezel), his friend and physician, risks his life when he ventures into Rome to visit him. Paul is under the watchful eye of Mauritius (Martinez), the prisons prefect, who seeks to understand how this broken old man can pose such a threat. But before Pauls death sentence can be enacted, Luke resolves to write another book, one that details the beginnings of The Way and the birth of what will come to be known as the church. Their faith challenged an empire. But their words changed the world.' }
   ];
 }
